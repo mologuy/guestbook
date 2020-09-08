c4815424d79d22ae386f1c42272e67a73497b349
mologuy <70864848+mologuy@users.noreply.github.com>
2020-09-07 22:13:14 -0500

    ejs files

--- app/app.js
+++ app/app.js
@@ -5,9 +5,15 @@ const fs = require('fs');
 
 app = express();
 
+dburi = JSON.parse(fs.readFileSync('secret.dburis.json'))[0];
+console.log(dburi);
+
 app.set('view engine', 'ejs');
 app.get('/', function(req, res){
-    res.render('index', {test1: 'test'});
+    res.render('index', {test: 'This is a test'});
 })
+
+//serve the rest of the files, located in public
+app.use(express.static('public'));
 
 module.exports = app;
