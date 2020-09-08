04c8190b510e7c596e87ae024a1c67a689601409
mologuy <70864848+mologuy@users.noreply.github.com>
2020-09-07 20:30:38 -0500

    basic ejs

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const fs = require('fs');
 
 app = express();
 
+app.set('view engine', 'ejs');
 app.get('/', function(req, res){
-    res.send('Express App');
+    res.render('index', {test1: 'test'});
 })
 
 module.exports = app;
--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./app/app.js');
 const port = process.env.PORT || 3000;
 
 app.listen(port, function(){
-    console.log(`listening on port ${port}`)
+    console.log(`listening on port ${port}`);
 });
