8c5a1d1046115e44386d84e6f83745538f5f2588
mologuy <70864848+mologuy@users.noreply.github.com>
2020-09-08 22:06:26 -0500

    added form functionality

--- app/app.js
+++ app/app.js
@@ -1,38 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const {Entry} = require('./db');
+const { Entry } = require('./db');
 
 app = express();
 
 //serve dynamic frontpage with all the entries (max: 255)
 app.set('view engine', 'ejs');
-app.get('/', function(req, res){
-    Entry.find().sort({date: 'desc'}).limit(255)
-    .then(function(results){
-        res.render('index', {entries: results, error: false});
-    })
-    .catch((err)=>{
-        console.error.bind(console, err);
-        res.render('index', {entries: [], error: true});
-    });
+app.get('/', function (req, res) {
+    Entry.find().sort({ date: 'desc' }).limit(255)
+        .then(function (results) {
+            res.render('index', { entries: results, error: false });
+        })
+        .catch((err) => {
+            console.error.bind(console, err);
+            res.render('index', { entries: [], error: true });
+        });
 })
 
 //Add new entries by POST, using an html form
+//expects name and body fields
 app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/submit', function(req, res){
-    var newentry = new Entry({
-        name: req.body.name,
-        body: req.body.body
-    });
-    newentry.save()
-    .then(function(entry){
-        console.log(entry);
-    })
-    .catch(function(err){
+app.post('/submit', function (req, res) {
+    try {
+        //trimming fields
+        req.body.body.trim();
+        req.body.name.trim();
+        //checking if name exists
+        if(req.body.name == '')
+            throw new Error('name is empty');
+        var newentry = new Entry({
+            name: req.body.name,
+            body: req.body.body,
+            date: new Date().now
+        });
+        //if body empty, make it null
+        if (req.body.body == '')
+            newentry.body = null;
+        
+        newentry.save()
+            .then(function (entry) {
+                console.log(entry);
+                res.redirect('/');
+            })
+            .catch(function (err) {
+                throw err;
+            });
+    }
+    catch(err) {
         console.log(err);
-    });
-    res.send(JSON.stringify(newentry));
+        res.status(500).send(`<h1>HTTP Error 500</h1>`);
+    }
 });
 
 //serve the rest of the files, located in public
